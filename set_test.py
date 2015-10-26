ac27edd78f2946d96cda2a71d36dce24b45ef9f2
Sher Minn Chong <sherminn.chong@gmail.com>
2015-10-26 17:10:44 -0400

    Remove comments

--- set_test.py
+++ set_test.py
@@ -1,44 +1,5 @@
-#!/usr/bin/env python
-###############################################################################
-#
-# set_test.py - Description!
-#
-###############################################################################
-# #
-# This program is free software: you can redistribute it and/or modify #
-# it under the terms of the GNU General Public License as published by #
-# the Free Software Foundation, either version 3 of the License, or #
-# (at your option) any later version. #
-# #
-# This program is distributed in the hope that it will be useful, #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the #
-# GNU General Public License for more details. #
-# #
-# You should have received a copy of the GNU General Public License #
-# along with this program. If not, see <http://www.gnu.org/licenses/>. #
-# #
-###############################################################################
-
-__author__ = "Miriam Shiffman"
-__copyright__ = "Copyright 2015"
-__credits__ = ["Miriam Shiffman"]
-__license__ = "GPL3"
-__version__ = "0.0.1"
-__maintainer__ = "Miriam Shiffman"
-__email__ = ""
-__status__ = "Development"
-
-###############################################################################
-
 import random
 import itertools
-
-###############################################################################
-###############################################################################
-###############################################################################
-###############################################################################
-
 
 class game:
     def __init__(self, cards=None):
@@ -77,15 +38,5 @@ class game:
             return results
         return False
 
-###############################################################################
-###############################################################################
-###############################################################################
-###############################################################################
-
 def test(n):
     return '{}%'.format(sum( game().play() for _ in xrange(n) )/float(n) * 100.0)
-
-##############################################################################
-###############################################################################
-###############################################################################
-###############################################################################
